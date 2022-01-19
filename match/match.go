57dcabe125bbe044b2450d1e5459e2fb5d843824
Andresen <eric@andresen.codes>
2022-01-19 22:11:55 +0100

    Added base set and leg when creating a match. Added function to get a match by id. Added implementation to add a throw to a given leg.

--- match/match.go
+++ match/match.go
@@ -1,6 +1,7 @@
 package match
 
 import (
+	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/guregu/dynamo"
@@ -18,11 +19,13 @@ type Match struct {
 }
 
 type Set struct {
+	Id     string `dynamo:"id"`
 	Legs   []Leg  `dynamo:"legs"`
 	Winner string `dynamo:"winner"`
 }
 
 type Leg struct {
+	Id     string  `dynamo:"id"`
 	Throws []Throw `dynamo:"throws"`
 	Winner string  `dynamo:"winner"`
 }
@@ -30,6 +33,12 @@ type Leg struct {
 type Throw struct {
 	UserId string `dynamo:"user"`
 	Score  int    `dynamo:"score"`
+}
+
+type ThrowIdentity struct {
+	MatchId string
+	SetId   string
+	LegId   string
 }
 
 type Service struct {
@@ -46,7 +55,44 @@ func NewService(tableName, region string) *Service {
 
 func (s Service) Create(match Match) (Match, error) {
 	match.Id = CreateId()
+	leg := Leg{
+		Id: CreateId(),
+	}
+	set := Set{
+		Id:   CreateId(),
+		Legs: []Leg{leg},
+	}
+	match.Sets = append(match.Sets, set)
 	return match, s.table.Put(match).Run()
+}
+
+func (s Service) GetById(matchId string) (match Match, err error) {
+	err = s.table.Get("id", matchId).One(&match)
+	if err != nil {
+		return Match{}, err
+	}
+	return
+}
+
+func (s Service) AddThrow(throw Throw, identity ThrowIdentity) (Throw, error) {
+	m, err := s.GetById(identity.MatchId)
+	if err != nil {
+		return Throw{}, errors.New("unable to find given match by id")
+	}
+	for _, set := range m.Sets {
+		if set.Id == identity.SetId {
+			for _, leg := range set.Legs {
+				if leg.Id == identity.LegId {
+					leg.Throws = append(leg.Throws, throw)
+				} else {
+					return Throw{}, errors.New("unable to find given leg in this match")
+				}
+			}
+		} else {
+			return Throw{}, errors.New("unable to find given set in this match")
+		}
+	}
+	return throw, s.table.Update("id", m.Id).Set("match", m).Value(&m)
 }
 
 func CreateId() (id string) {
