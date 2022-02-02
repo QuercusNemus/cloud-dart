7985bb8f35daf5bbba3668acaf26645a0f17e33b
Andresen <eric@andresen.codes>
2022-02-02 21:58:30 +0100

    Fixed id to match the new name in the dynamo table.

--- player/player.go
+++ player/player.go
@@ -11,7 +11,7 @@ import (
 type Player struct {
 	Age      int      `dynamo:"age"`
 	Email    string   `dynamo:"email"`
-	Id       string   `dynamo:"id"`
+	Id       string   `dynamo:"player_id"`
 	Name     string   `dynamo:"name"`
 	NickName string   `dynamo:"nick_name"`
 	Matches  []string `dynamo:"matches"`
