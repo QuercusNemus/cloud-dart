c438dbf538b7a2c1d0b8d3d89e6af3c178ff2a61
Andresen <eric@andresen.codes>
2022-02-02 21:59:23 +0100

    Fixed typo where the lambda trying  to call a not known dynamo table.

--- player/lambda/create/main.go
+++ player/lambda/create/main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"cloud-dart/player"
 	"context"
-	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 var playerService *player.Service
 
 func init() {
-	playerService = player.NewService("Player", "eu-north-1")
+	playerService = player.NewService("Players", "eu-north-1")
 }
 
 func handler(ctx context.Context, player player.Player) (player.Player, error) {
-	fmt.Println("CreatePlayer invoked!")
 	return playerService.Create(player)
 }
 
